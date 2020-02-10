173d9f3d2caa1795bc56725e2d41bde5b9acabb3
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-02-10 13:21:10 -0300

    Curso DIO -  Aprenda o que é jQuery e Bootstrap

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 // alert("funcionou");
 
 function consultaCep() {
+    $(".barra-progresso").show();
     var cep = document.getElementById("cep").value;
     var url = "https://viacep.com.br/ws/" + cep +"/json/";
     $.ajax({
         url: url,
         type: "GET",
         success: function(response) {
-            console.log(response);
-            $("#logradouro").html(response.logradouro)
-            // document.getElementById("logradouro").innerHTML = response.logradouro;
-            document.getElementById("bairro").innerHTML = response.bairro;
+            $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
             document.getElementById("localidade").innerHTML = response.localidade;
             document.getElementById("uf").innerHTML = response.uf;
+            $("#titulo_cep").html("CEP: " + response.cep);
+            $(".cep").show();
+            $(".barra-progresso").hide();
         }
     })
 }
+
+$(function(){
+    $(".cep").hide();
+    $(".barra-progresso").hide();
+})
